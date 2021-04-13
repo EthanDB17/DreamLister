df2eb3f90b13bd9d4c9cd8468e9af3534ebd67f5
Ethan Borrowman <ethan_borrowman@hotmail.com>
2021-04-12 23:00:44 -0400

    Adding view and view controller for add/edit item page

---
+++ DreamLister/Controller/ItemDetailsVC.swift
@@ -0,0 +1,31 @@
+//
+//  ItemDetailsVC.swift
+//  DreamLister
+//
+//  Created by Ethan Borrowman on 4/12/21.
+//
+
+import UIKit
+
+class ItemDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
